fad559b419cc919e44a73cecc2e323f55ec89ed3
techikansh <106448256+techikansh@users.noreply.github.com>
2024-08-16 01:09:57 +0200

    fixed getInitials!

--- Client/notesApp/src/utils/helper.js
+++ Client/notesApp/src/utils/helper.js
@@ -1,6 +1,5 @@
 export const getInitials = (name) => {
-    name = "Devansh Kumar";
-    sepNames = name.split(" ");
+    const sepNames = name.split(" ");
     return sepNames[0].charAt(0) + sepNames[1].charAt(0);
     
 }
